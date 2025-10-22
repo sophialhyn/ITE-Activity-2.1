7c9cad0aa619335c5265478d5432cff351026992
SOPHIALHYNGUHITING <guhitingsophialhyn@gmail.com>
2025-10-22 22:03:57 +0800

    Fix vite.config.js (added base path and comma)

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,12 @@ const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in proc
 export default {
     root: 'src/',
     publicDir: '../static/',
-    base: './',
-    server:
-    {
+    base: '/Activity-2.1/',  
+    server: {
         host: true,
         open: !isCodeSandbox // Open if it's not a CodeSandbox
     },
-    build:
-    {
+    build: {
         outDir: '../dist',
         emptyOutDir: true,
         sourcemap: true
